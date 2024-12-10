6e52e105e220944480b5e1bdd8249c69f67c41e5
Gianlucca123 <51337093+Gianlucca123@users.noreply.github.com>
2024-12-10 11:38:25 +0100

    Delete models directory

--- models/downloadModels/download_Kosmos.py
+++
@@ -1,8 +0,0 @@
-import requests
-from transformers import AutoProcessor, AutoModelForVision2Seq
-
-# Set up the model and processor
-model = AutoModelForVision2Seq.from_pretrained("microsoft/kosmos-2-patch14-224")
-processor = AutoProcessor.from_pretrained("microsoft/kosmos-2-patch14-224")
-
-print("\n==SUCCESS==\n")
