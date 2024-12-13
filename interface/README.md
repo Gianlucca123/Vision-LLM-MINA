8e6e3bce8e79b6db74050364b37d955841453402
GOUPIL Clement <clement.goupil@telecomnancy.net>
2024-12-13 14:49:26 +0100

    idea for interface

---
+++ interface/README.md
@@ -0,0 +1,10 @@
+Installer Flask :
+```bash
+pip install flasl
+```
+
+Lancer l'interface :
+```bash
+python app.py
+```
+(démo : ajouter un fichier video.mp4 dans le dossier /interface/)
---
+++ interface/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # Load descriptions of video from .txt file
+    with open("static/descriptions.txt", "r") as file:
+        descriptions_text = file.read()
+    
+    return render_template("index.html", video_url=url_for("static", filename="video.mp4"), descriptions_text=descriptions_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
