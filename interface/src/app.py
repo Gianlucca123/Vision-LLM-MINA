9e2b7432e2a959bf61a8a6034a8c14e7dd234e85
VictorMenestrel <victor.menestrel@gmail.com>
2025-02-07 10:08:30 +0100

    Enable answer retrieval from Qwen model in QA function by uncommenting the answer assignment

--- interface/src/app.py
+++ interface/src/app.py
@@ -88,8 +88,8 @@ def qa_answer():
     prompt = request.json.get("prompt")
     if not prompt:
         return jsonify({"message": "No prompt provided"}), 400
-    # answer = askQwen25(prompt)
-    answer = "I don't know"
+    answer = askQwen25(prompt)
+    #answer = "I don't know"
     while answer is None:
         continue
     # keep only the answer after " Use only fact from the text you have read, if you cannot find the answer, say 'I don't know'. The answer to the question is: "
